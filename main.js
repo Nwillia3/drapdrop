cc198151851e2e0e6b9e24b312a5c5104c426702
Neil Williams <Neil.Williams227@gmail.com>
2018-10-24 06:43:30 -0400

    completed basic drop and drop

--- main.js
+++ main.js
@@ -6,7 +6,37 @@ const empties = document.querySelectorAll(".empty");
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
 
-//Drag Functions
-function dragStart() {}
+//loop through empties and call drag event
 
-function dragEnd() {}
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
+//Drag Functions
+function dragStart() {
+  this.className += " hold";
+  setTimeout(() => {
+    this.className = "invisible";
+  }, 0);
+}
+
+function dragEnd() {
+  this.className = "fill";
+}
+
+function dragOver(e) {
+  e.preventDefault(); // to prevent the default behavior in order for the dragDrop to run
+}
+function dragEnter(e) {
+  e.preventDefault(); // to prevent the default as first
+  this.className += " hovered"; // this append to the class
+}
+function dragLeave() {
+  this.className = "empty"; // this replaces the exisitng class name
+}
+function dragDrop() {
+  this.append(fill); // adds the entire element
+}
