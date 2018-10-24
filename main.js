829c4cd1992c1a9880186fa36690c8f98c1eb632
Neil Williams <Neil.Williams227@gmail.com>
2018-10-24 06:19:55 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+//fill listeners
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+//Drag Functions
+function dragStart() {}
+
+function dragEnd() {}
